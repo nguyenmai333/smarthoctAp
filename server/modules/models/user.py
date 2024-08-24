a878074eff2e1aa9985124c09b084b01ede01de8
Dan Nguyen <thanhdanhvc1403@gmail.com>
2024-08-24 23:52:39 +0700

    update id text

--- server/modules/models/user.py
+++ server/modules/models/user.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from datetime import datetime
+from uuid import UUID, uuid4
 
 class ProcessedText(BaseModel):
+    id: UUID = uuid4()
     text: str
     date: datetime
 
