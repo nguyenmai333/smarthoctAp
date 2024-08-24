a7975082fb9c9b6cb2472cb7d1b31b93a2951e77
korrotta <hoaihieulam@gmail.com>
2024-08-24 23:27:30 +0700

    Added Img2Text UI & Logic

--- app/app/src/main/java/com/uitcontest/studymanagement/api/ApiClient.java
+++ app/app/src/main/java/com/uitcontest/studymanagement/api/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://172.26.250.122:3550/docs/";
+    private static final String BASE_URL = "http://172.26.52.95:8000/docs/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
@@ -20,6 +20,10 @@ public class ApiClient {
             Log.d("ApiClient", "Reusing existing Retrofit instance");
         }
         return retrofit;
+    }
+
+    public static ApiService getApiService() {
+        return getClient().create(ApiService.class);
     }
 }
 
