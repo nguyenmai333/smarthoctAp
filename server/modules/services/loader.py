a240477d2fa707efd3b7c5021993ac8a962d1ce3
leam <125895598+IamLeam@users.noreply.github.com>
2024-08-25 01:26:51 +0700

    update creat_mindmap and process_func
    t mới test trên notebook thôi, thằng nào host đc thì test api hộ

--- server/modules/services/loader.py
+++ server/modules/services/loader.py
@@ -1,6 +1,6 @@
 import spacy
 import pkg_resources
-from transformers import AutoTokenizer,AutoModel,AutoModelForSeq2SeqLM,pipeline
+from transformers import AutoTokenizer,AutoModel,AutoModelForSeq2SeqLM,pipeline, AutoModelForSequenceClassification
 
 
 phoBert_tokenizer = AutoTokenizer.from_pretrained(pkg_resources.resource_filename('modules', 'pretrain_models/phobert-base'))
@@ -16,3 +16,9 @@ envit5_tokenizer = AutoTokenizer.from_pretrained(pkg_resources.resource_filename
 envit5_model = AutoModelForSeq2SeqLM.from_pretrained(pkg_resources.resource_filename('modules', 'pretrain_models/envit5-translation'))
 
 sentence_tokenizer = spacy.load(pkg_resources.resource_filename('modules', 'pretrain_models/en_core_web_sm'))
+
+mnli_tokenizer = AutoTokenizer.from_pretrained(pkg_resources.resource_filename('modules', 'pretrain_models/bart-large-mnli'))
+mnli_model = AutoModelForSequenceClassification.from_pretrained(pkg_resources.resource_filename('modules', 'pretrain_models/bart-large-mnli'))
+
+Bert_tokenizer = AutoTokenizer.from_pretrained(pkg_resources.resource_filename('modules', 'pretrain_models/bert-base-uncased'))
+Bert_model = AutoModel.from_pretrained(pkg_resources.resource_filename('modules', 'pretrain_models/bert-base-uncased'))
--- server/modules/services/process_func.py
+++ server/modules/services/process_func.py
@@ -1,9 +1,10 @@
 import torch
 
-from modules.services.loader import sentence_tokenizer,phoBert_model,phoBert_tokenizer
+from modules.services.loader import sentence_tokenizer,phoBert_model,phoBert_tokenizer, mbart_tokenizer, mbart_model, mnli_tokenizer, mnli_model, Bert_tokenizer, Bert_model, envit5_tokenizer, envit5_model
 from sklearn.cluster import KMeans
 from collections import defaultdict
 import torch
+import torch.nn.functional as F
 
 def extract_key_sentences(text): 
     doc = sentence_tokenizer(text)
@@ -11,7 +12,7 @@ def extract_key_sentences(text):
     return sentences
 
 def cluster_sentences(sentences, n_clusters):
-    X = torch.vstack([get_embedding(sent, phoBert_tokenizer, phoBert_model) for sent in sentences]).numpy()
+    X = torch.vstack([get_embedding(sent, Bert_tokenizer, Bert_model) for sent in sentences]).numpy()
     kmeans = KMeans(n_clusters=n_clusters, random_state=0)
     clusters = kmeans.fit_predict(X)
     clustered_sentences = defaultdict(list)
@@ -19,6 +20,7 @@ def cluster_sentences(sentences, n_clusters):
         clustered_sentences[cluster].append(sentence)
     clustered_paragraphs = [" ".join(clustered_sentences[cluster]) for cluster in sorted(clustered_sentences)]
     return clustered_paragraphs
+
 def get_embedding(text, tokenizer, model):
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512)
     with torch.no_grad():
@@ -48,3 +50,45 @@ def translate_text(text, model, tokenizer):
     inputs = tokenizer.encode(text, return_tensors="pt", max_length=max_input_length, truncation=True)
     outputs = model.generate(inputs, max_length=max_output_length, num_beams=4, early_stopping=True)
     return tokenizer.decode(outputs[0][3:], skip_special_tokens=True)
+
+
+
+def summarize_text(text):
+    inputs = mbart_tokenizer.encode(text, return_tensors="pt", max_length=1024, truncation=True)
+    outputs = mbart_model.generate(inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = mbart_tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return summary
+
+def summarize_paragraphs(paragraphs):
+    summaries = []
+    for paragraph in paragraphs:
+        summary = summarize_text(paragraph)
+        summaries.append(summary)
+    return summaries
+
+def translate_paragraphs(paragraphs):
+    translated_paragraphs = []
+    for paragraph in paragraphs:
+        translated_text = translate_text(paragraph, envit5_model, envit5_tokenizer)
+        translated_paragraphs.append(translated_text)
+    return translated_paragraphs
+
+labels = ["Math", "Science", "Data", "Computers", "Social", "Physics", "History"] 
+def classify_paragraph(paragraph):
+    inputs = mnli_tokenizer(paragraph, return_tensors="pt", truncation=True, padding=True)
+    with torch.no_grad():
+        outputs = mnli_model(**inputs)
+    logits = outputs.logits
+    probs = F.softmax(logits, dim=1)
+    max_prob, predicted_label_index = torch.max(probs, dim=1)
+    if max_prob.item() < 0.8:
+        return "Other"
+    predicted_label = labels[predicted_label_index.item()]
+    return predicted_label
+
+def classify_paragraphs(paragraphs):
+    topic_assignments = []
+    for paragraph in paragraphs:
+        topic = classify_paragraph(paragraph)
+        topic_assignments.append(topic)
+    return topic_assignments
