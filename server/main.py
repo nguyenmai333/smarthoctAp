90e538c49841dab29065e052c4ff827386b9541a
Dan Nguyen <thanhdanhvc1403@gmail.com>
2024-09-10 15:45:06 +0700

    update dockerfile and main

--- server/main.py
+++ server/main.py
@@ -9,11 +9,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Include routers
 app.include_router(user.router)
-# app.include_router(translation.router)
-# app.include_router(summarization.router)
-# app.include_router(smart_search.router)
-# app.include_router(speech2text.router)
-# app.include_router(genQA.router)
+app.include_router(translation.router)
+app.include_router(summarization.router)
+app.include_router(smart_search.router)
+app.include_router(speech2text.router)
+app.include_router(genQA.router)
 app.include_router(image_routes.router)
 
 
