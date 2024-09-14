0ace3ebefbd136315b1329401d5858a80df28c6d
Dan Nguyen <thanhdanhvc1403@gmail.com>
2024-09-15 01:13:18 +0700

    update deloy

--- server/modules/core/database.py
+++ server/modules/core/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from config import settings
 
-client = MongoClient(settings.database_url, username=settings.mongo_initdb_root_username, password=settings.mongo_initdb_root_password, uuidRepresentation='standard')
+client = MongoClient(f'mongodb://{settings.username}:{settings.password}@{settings.host}:{settings.port}', uuidRepresentation='standard')
 
 db = client.mydatabase
 users_collection = db.users
