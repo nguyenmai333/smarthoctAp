ab6e7bc3950bf7da52265960f9091c82247abd0f
Dan Nguyen <thanhdanhvc1403@gmail.com>
2024-09-18 12:17:19 +0700

    update modules

--- server/modules/routes/user.py
+++ server/modules/routes/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from modules.models.user import UserInDB
+from modules.models.user import UserInDB,UserCreate
 from modules.core.auth import get_password_hash, create_access_token, verify_password, get_current_user
 from modules.core.database import users_collection
 from datetime import timedelta
@@ -9,13 +9,16 @@ router = APIRouter()
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 @router.post("/register/")
-async def register_user(username: str, password: str, email: str, full_name: str):
-    existing_user = users_collection.find_one({"username": username})
+async def register_user(user: UserCreate):
+    existing_user = users_collection.find_one({"username": user.username})
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already registered")
-    hashed_password = get_password_hash(password)
-    user = UserInDB(username=username, email=email, full_name=full_name, hashed_password=hashed_password)
-    users_collection.insert_one(user.dict())
+    
+    hashed_password = get_password_hash(user.password)
+    user_data = user.dict()
+    user_data["hashed_password"] = hashed_password
+    users_collection.insert_one(user_data)
+    
     return {"message": "User created successfully"}
 
 @router.post("/token")
